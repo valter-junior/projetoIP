4f12b7e4380daddb2111c9b8a8b894a758ae7436
Valter Junior <valtrjose.sjr@gmail.com>
2021-12-15 10:16:08 -0300

    Update files

--- classes/enemyClass.py
+++ classes/enemyClass.py
@@ -1,11 +1,18 @@
 import pygame
+import random
 
 
 class Enemy:
-    image = pygame.image.load("/home/valter/projetoIP/images/enemy.jpeg")
 
-    def __init__(self, name, velo, x, y):
-        self.name = name
-        self.velo = velo
-        self.x = x
-        self.y = y
+    def __init__(self, image, TELA, NAVE):
+        self.image = image
+        self.tamanho = self.image.get_rect().size
+        self.TELA = TELA
+        self.tela = TELA.get_react().size
+        self.rect = self.image.get_rect(
+            center=(
+                random.randint(self.tela[0] + 20, self.tela[0] + 100),
+                random.randint(0, self.tela[1])
+            )
+        )
+        self.speed = random.randint(3, 10)
